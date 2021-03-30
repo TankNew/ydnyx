32bcd78714da48a045ac949041fc84795e4f965e
TankNew <tanknew@outlook.com>
2021-03-30 13:20:20 +0800

    20210330

--- pages/index.vue
+++ pages/index.vue
@@ -10,14 +10,14 @@
 import axios from 'axios'
 import Logo from '~/components/Logo.vue'
 export default {
-    components: {
-        Logo
-    },
-    async fetch(context) {
-        const ip = (await axios.get('http://icanhazip.com')).data
-        context.store.dispatch('setIp', ip)
-        context.redirect('/zh-CN/home')
-        // The fetch method is used to fill the store before rendering the page
-    }
+  components: {
+    Logo
+  },
+  fetch(context) {
+    // const ip = (await axios.get('http://icanhazip.com')).data
+    // context.store.dispatch('setIp', ip)
+    context.redirect('/zh-CN/home')
+    // The fetch method is used to fill the store before rendering the page
+  }
 }
 </script>
